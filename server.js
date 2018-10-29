568d5f83baa21d32e7cfd29febf805f8e6dd2ab5
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-10-29 23:58:59 +0530

    Added Feature to save template.

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const fs = require('fs');
 
 app.use(express.static('static_files'));
 // parse application/json
 app.use(bodyParser.json());
+
+let template = '';
+
+template = fs.readFileSync('./template.html', 'UTF-8');
+
+//console.log(template);
 
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -14,6 +21,21 @@ const msg = {
   from: 'vks18765@gmail.com',
   subject: 'Sending with SendGrid is Fun'
 };
+
+app.get('/template', (req, res) => {
+  res.send({template: template})
+});
+
+app.post('/template', (req, res) => {
+  //console.log(req.body.template);
+  template = req.body.template;
+  fs.writeFile('template.html', template, function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+},(err) => {
+  console.log("Error to save template");
+});
 
 app.post('/sendMail', (req, res) => {
   console.log('Post Successful');
